86a3339a5d9ca609665834169cacdcac3014121b
TaNviiR-RiiYaD <80155708+TaNviiR-RiiYaD@users.noreply.github.com>
2021-05-08 12:08:19 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,34 @@
+apt  update
+apt  upgrade
+apt  install python
+apt  install git
+apt  install python2
+apt  install nano
+apt  install wget
+apt  install curl
+apt  install php
+apt  install ruby
+apt  install figlet
+apt  install cmatrix
+apt  install openssh
+pip  install requests
+pip2 install requests
+pip  install mechanize
+pip2 install mechanize
+pip2 install bs4
+apt  install toilet
+termux-setup-storage
+pip2 install bkey
+bkey
+bkey
+clear
+pip install pytube
+pip install pyfiglet
+pip install termcolor
+echo
+echo
+echo "now your Terminal is ready for Run any Tools"
+echo "Powered by TaNviiR-RiiYaD"
+echo
+echo
+cd
